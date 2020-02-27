e4caeda91669783161198ae1e4a66045314bc2ee
MichaelJereza <mdj1999@gmail.com>
2020-02-26 23:11:23 -0800

    Refactored Code

--- Bus.hs
+++ Bus.hs
@@ -17,18 +17,40 @@ data Route = Halt Stop
 --          | Map [Route]        
 --handleTasks :: [Task]->Bus
 
-
+-- | Exchange the passengers from stop and bus capacity.
 performStop :: Stop->Bus->Bus
 performStop stop (passengers, max) = case stop of
-                       Gain cap -> if passengers+cap<max then (passengers+cap, max) else (max, max)
-                       Loss cap -> if passengers-cap>0 then (passengers-cap, max) else (0, max)
+                       Gain cap -> if (busHandle (passengers, max) cap) then (passengers+cap, max) else (max, max)
+                       Loss cap -> if (busHandle (passengers, max) (-cap)) then (passengers-cap, max) else (0, max)
 
+
+
+-- | Bus handles each stop in a [Route].
 schedule :: Bus -> [Route] -> Bus 
 schedule bus [] = bus
 schedule bus (r:oute) = case r of
                         Halt stop   -> schedule (performStop stop bus) oute
                         Go distance -> schedule bus oute
 
+
+
+-- | Functions about Bus state
+--
+-- Is the bus full?
+busFull :: Bus -> Bool
+busFull (passengers, max) = if passengers == max then True else False
+
+-- Is the bus empty?
+busEmpty :: Bus -> Bool
+busEmpty (passengers, max) = if passengers == 0 then True else False
+
+-- Do passenger exchanges make sense?
+busHandle :: Bus -> Capacity -> Bool
+busHandle (passengers, max) netchange = if ( (passengers + netchange <= max) && (passengers + netchange >= 0) ) then True else False
+
+
+
+-- | Testing examples
 exampleRoute :: [Route]
 exampleRoute = [Go 15, Halt (Gain 1), Go 5, Halt (Gain 10), Go 20, Halt (Loss 5)]
 

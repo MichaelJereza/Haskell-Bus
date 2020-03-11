4399b4f447ecf75c88100176b1c18ee8c079181b
MichaelJereza <mdj1999@gmail.com>
2020-03-11 15:44:27 -0700

    Fixed printing, if/while, added infiniteTask

--- Bus.hs
+++ Bus.hs
@@ -16,18 +16,25 @@ data Command = Halt Stop
              | Go Miles
              deriving(Eq,Show)
 data Task = Run Route
-          | While Status Route
-          | If Status Route Route
+          | While Status Task
+          | If Status Task Task
+
+
+-- | This is the main wrapper for handling tasks and running the program.
+displayTasks :: Bus->[Task]->IO ()
+displayTasks bus tasks = putStrLn ("Starting: " ++ show bus ++ "." ++ handleTasks bus tasks)
 
 
 
-handleTasks :: Bus->[Task]->Bus
-handleTasks bus [] = bus
-handleTasks bus ((Run route):s) = handleTasks (schedule bus route) s
-handleTasks bus ((While condition route):s) = case (condition bus) of
-                                              True -> handleTasks (schedule bus route) ((While condition route):s)
-                                              False -> handleTasks (schedule bus route) s
-handleTasks bus ((If status t e):s) = if (status bus) then handleTasks (schedule bus t) s else handleTasks (schedule bus e) s
+-- | Iterates through Task list and returns strings of operations.
+handleTasks :: Bus->[Task]->String
+handleTasks bus [] = "\nFinished: " ++ show bus ++ "."
+handleTasks bus ((Run route):s) = case schedule bus route of 
+                                  bus' -> "\nRan route: " ++ show bus' ++ "." ++ handleTasks (schedule bus route) s
+handleTasks bus ((While condition task):s) = case (condition bus) of
+                                              True -> "\nWhile: True..." ++ handleTasks bus (task:(While condition task):s)
+                                              False -> "\nWhile: False..." ++ handleTasks bus s
+handleTasks bus ((If status t e):s) = if (status bus) then "\nIf: True..." ++ handleTasks bus (t:s) else "\nIf: False..." ++ handleTasks bus (e:s)
 
 
 
@@ -72,20 +79,30 @@ busHandle (passengers, max) netchange = if ( (passengers + netchange <= max) && 
 
 
 -- | Testing examples
+-- Gain 5
 exampleRoute :: Route
-exampleRoute = [Go 15, Halt ("Street A", (Gain 1)), Go 5, Halt ("Street B", (Gain 5)), Go 20, Halt ("Street C", (Loss 1))]
-
+exampleRoute = [Go 15, Halt ("Street A", (Gain 1)), Go 5, Halt ("Street B", (Loss 1)), Go 20, Halt ("Street C", (Gain 5))]
+-- Lose 1
 exampleRoute2 :: Route
 exampleRoute2 = [Halt ("Street D", (Loss 1))]
 
 exampleRun1 :: Bus
 exampleRun1 = schedule (0, 30) exampleRoute
 
-exampleTasks :: Bus
-exampleTasks = handleTasks (0, 20) [Run exampleRoute, While busHasPassengers exampleRoute2 ]
+-- Fills 5 passengers, loops emptying 1 passenger at a time
+exampleTask1 :: IO ()
+exampleTask1 = displayTasks (0, 20) [Run exampleRoute, While busHasPassengers (Run exampleRoute2) ]
+
+-- Fills bus, then loops emptying 1 passenger at a time
+exampleTask2 :: IO ()
+exampleTask2 = displayTasks (1, 20) [While busHasPassengers (If busFull (While busHasPassengers (Run exampleRoute2)) (Run exampleRoute))] 
+
+-- An example of a failure, looping forever.
+infiniteTask :: IO ()
+infiniteTask = displayTasks (20, 20) [While busHasPassengers (If busFull (Run exampleRoute2)(Run exampleRoute))]
 
 exampleFail1 :: Bus
 exampleFail1 = schedule (5, 10) [Go 5, Halt ("Street A", (Gain 10))]
 
 exampleFail2 :: Bus
-exampleFail2 = schedule (0, 5) [Go 5, Halt ("Street Fart", (Loss 1))]
+exampleFail2 = schedule (0, 5) [Go 5, Halt ("Street B", (Loss 1))]

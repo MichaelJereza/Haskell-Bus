354bf0940b4eb0586e11de3a32726f6e1f1fc9c6
MichaelJereza <mdj1999@gmail.com>
2020-03-12 13:35:00 -0700

    Variable definition

--- Bus.hs
+++ Bus.hs
@@ -16,26 +16,55 @@ data Command = Halt Stop
              | Go Miles
              deriving(Eq,Show)
 data Task = Run Route
+          | Create Name Route
+          | Use Name
           | While Status Task
           | If Status Task Task
 
+type Var = (Name, Route)
 
 -- | This is the main wrapper for handling tasks and running the program.
 displayTasks :: Bus->[Task]->IO ()
-displayTasks bus tasks = putStrLn ("\nStarting: " ++ show bus ++ ".\n" ++ handleTasks bus tasks)
+displayTasks bus tasks = putStrLn ("\nStarting: " ++ show bus ++ ".\n" ++ handleTasks bus tasks empty)
+
+
+
+-- | Initial Variable environment called by displayTasks
+empty :: [Var]
+empty = []
 
 
 
 -- | Iterates through Task list and returns strings of operations.
-handleTasks :: Bus->[Task]->String
-handleTasks bus [] = "\n\nFinished: " ++ show bus ++ ".\n"
-handleTasks bus ((Run route):s) = case schedule bus route of 
-                                  bus' -> "\nRunning route: " ++ rootInfo route ++ "\nBus: " ++ show bus' ++ "." ++ handleTasks (schedule bus route) s
-handleTasks bus ((While condition task):s) = case (condition bus) of
-                                             True  -> "\nWhile: True..." ++ handleTasks bus (task:(While condition task):s)
-                                             False -> "\nWhile: False..." ++ handleTasks bus s
-handleTasks bus ((If status t e):s) = if (status bus) then "\nIf: True..." ++ handleTasks bus (t:s) 
-                                                      else "\nIf: False..." ++ handleTasks bus (e:s)
+handleTasks :: Bus->[Task]->[Var]->String
+handleTasks bus [] env = "\n\nFinished: " ++ show bus ++ ".\n"
+handleTasks bus ((Run route):s) env = case schedule bus route of 
+                                      bus' -> "\nRunning route: " ++ rootInfo route ++ "\nBus: " ++ show bus' ++ "." ++ handleTasks (schedule bus route) s env
+handleTasks bus ((While condition task):s) env = case (condition bus) of
+                                             True  -> "\nWhile: True..." ++ handleTasks bus (task:(While condition task):s) env
+                                             False -> "\nWhile: False..." ++ handleTasks bus s env
+handleTasks bus ((If status t e):s) env = if (status bus) then "\nIf: True..." ++ handleTasks bus (t:s) env
+                                                      else "\nIf: False..." ++ handleTasks bus (e:s) env
+handleTasks bus ((Create name route):s) env = case getVar name env of
+                                              Just _ -> "\nFailed creating " ++ name ++ ", variables already exists!"
+                                              Nothing -> handleTasks bus s (env++[createVar name route])
+handleTasks bus ((Use name):s) env = case getVar name env of
+                                     Just route -> "\nRunning variable: " ++ name ++ "." ++ handleTasks bus ((Run route):s) env
+                                     Nothing -> "\nVariable " ++ name ++ " does not exist!"
+
+
+
+-- | Get Var from name return route
+getVar :: Name->[Var]->Maybe Route
+getVar name [] = Nothing
+getVar name ((var, route):variables) = if name == var then Just route else getVar name variables
+
+
+
+-- | Create Var from Name and Route
+createVar :: Name->Route->Var
+createVar name route = (name, route)
+
 
 
 -- | Return a string representing a route.
@@ -107,10 +136,17 @@ exampleTask1 = displayTasks (0, 20) [Run exampleRoute, While busHasPassengers (R
 exampleTask2 :: IO ()
 exampleTask2 = displayTasks (1, 20) [While busHasPassengers (If busFull (While busHasPassengers (Run exampleRoute2)) (Run exampleRoute))] 
 
+-- Defines "myroute" route
+exampleVariable1 :: IO ()
+exampleVariable1 = displayTasks (1, 20) [Create "myroute" [Go 15, Halt ("Stop Z", (Gain 1)), Go 3.5], While busHasRoom (Use "myroute")] 
+
+-- Defines "myroute" and "youroute"
+exampleVariable2 :: IO ()
+exampleVariable2 = displayTasks (1, 20) [Create "myroute" [Go 15, Halt ("Stop Z", (Gain 1)), Go 3.5], Create "youroute" [Go 1.2, Halt ("Stop X", (Loss 1)), Go 0.2], While busHasRoom (Use "myroute"), While busHasPassengers (Use "youroute")] 
+
 -- An example of a failure, looping forever.
 infiniteTask :: IO ()
 infiniteTask = displayTasks (20, 20) [While busHasPassengers (If busFull (Run exampleRoute2)(Run exampleRoute))]
-
 
 exampleFail1 :: Bus
 exampleFail1 = schedule (5, 10) [Go 5, Halt ("Stop A", (Gain 10))]

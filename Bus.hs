52c4daf8176f23a827f05ef7308a3b03b55a8574
MichaelJereza <mdj1999@gmail.com>
2020-03-12 13:42:53 -0700

    Added badVariable example

--- Bus.hs
+++ Bus.hs
@@ -47,7 +47,7 @@ handleTasks bus ((If status t e):s) env = if (status bus) then "\nIf: True..." +
                                                       else "\nIf: False..." ++ handleTasks bus (e:s) env
 handleTasks bus ((Create name route):s) env = case getVar name env of
                                               Just _ -> "\nFailed creating " ++ name ++ ", variables already exists!"
-                                              Nothing -> handleTasks bus s (env++[createVar name route])
+                                              Nothing -> "\nCreated " ++ name ++ "." ++ handleTasks bus s (env++[createVar name route])
 handleTasks bus ((Use name):s) env = case getVar name env of
                                      Just route -> "\nRunning variable: " ++ name ++ "." ++ handleTasks bus ((Run route):s) env
                                      Nothing -> "\nVariable " ++ name ++ " does not exist!"
@@ -143,6 +143,9 @@ exampleVariable1 = displayTasks (1, 20) [Create "myroute" [Go 15, Halt ("Stop Z"
 -- Defines "myroute" and "youroute"
 exampleVariable2 :: IO ()
 exampleVariable2 = displayTasks (1, 20) [Create "myroute" [Go 15, Halt ("Stop Z", (Gain 1)), Go 3.5], Create "youroute" [Go 1.2, Halt ("Stop X", (Loss 1)), Go 0.2], While busHasRoom (Use "myroute"), While busHasPassengers (Use "youroute")] 
+
+badVariable :: IO ()
+badVariable =  displayTasks (1, 20) [Create "myroute" [Go 15, Halt ("Stop Z", (Gain 1)), Go 3.5], Create "myroute" [Go 1.2, Halt ("Stop X", (Loss 1)), Go 0.2], If busHasRoom (Use "myroute") (Run exampleRoute)]
 
 -- An example of a failure, looping forever.
 infiniteTask :: IO ()
